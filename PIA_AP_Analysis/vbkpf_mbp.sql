d0868df27c4f870d936a77d3154a8c661f070441
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-27 20:36:43 +0530

    Creating PIA_AP_Analysis/vbkpf_mbp.sql

---
+++ PIA_AP_Analysis/vbkpf_mbp.sql
@@ -0,0 +1,17 @@
+
+                        -- Databricks notebook source
+                        CREATE  TABLE IF NOT EXISTS prod_l2.vbkpf_mbp
+                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/VBKPF_MBP'
+                        COMMENT 'vbkpf_mbp';
+
+                        ALTER TABLE  prod_l2.foundation.vbkpf_mbp
+                        SET TBLPROPERTIES (
+                          'delta.autoOptimize.optimizeWrite' = 'true',
+                          'delta.autoOptimize.autoCompact' = 'false',
+                          'delta.enableChangeDataFeed' = 'true',
+                          'delta.tuneFileSizesForRewrites' = 'true',
+                          'certified' = 'no',
+                          'pii_data'='no', 
+                          'primary_source' = 'yes',
+                          'sox_compliance' = 'no'
+                        ); 

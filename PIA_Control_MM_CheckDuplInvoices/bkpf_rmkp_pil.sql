22321340997a980141491d7b6b3955e38362fb69
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-27 21:31:23 +0530

    Creating PIA_Control_MM_CheckDuplInvoices/bkpf_rmkp_pil.sql

---
+++ PIA_Control_MM_CheckDuplInvoices/bkpf_rmkp_pil.sql
@@ -0,0 +1,17 @@
+
+                        -- Databricks notebook source
+                        CREATE  TABLE IF NOT EXISTS prod_l2.bkpf_rmkp_pil
+                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/BKPF_RMKP_PIL'
+                        COMMENT 'bkpf_rmkp_pil';
+
+                        ALTER TABLE  prod_l2.foundation.bkpf_rmkp_pil
+                        SET TBLPROPERTIES (
+                          'delta.autoOptimize.optimizeWrite' = 'true',
+                          'delta.autoOptimize.autoCompact' = 'false',
+                          'delta.enableChangeDataFeed' = 'true',
+                          'delta.tuneFileSizesForRewrites' = 'true',
+                          'certified' = 'no',
+                          'pii_data'='no', 
+                          'primary_source' = 'yes',
+                          'sox_compliance' = 'no'
+                        ); 

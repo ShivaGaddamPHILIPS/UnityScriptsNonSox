7becbee48ba8ce45eaeb03aded99050da7f49b13
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-27 20:34:20 +0530

    Creating PIA_AP_Analysis/skat_mp1.sql

---
+++ PIA_AP_Analysis/skat_mp1.sql
@@ -0,0 +1,17 @@
+
+                        -- Databricks notebook source
+                        CREATE  TABLE IF NOT EXISTS prod_l2.skat_mp1
+                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//POCA/QVDs/SKAT_MP1'
+                        COMMENT 'skat_mp1';
+
+                        ALTER TABLE  prod_l2.foundation.skat_mp1
+                        SET TBLPROPERTIES (
+                          'delta.autoOptimize.optimizeWrite' = 'true',
+                          'delta.autoOptimize.autoCompact' = 'false',
+                          'delta.enableChangeDataFeed' = 'true',
+                          'delta.tuneFileSizesForRewrites' = 'true',
+                          'certified' = 'no',
+                          'pii_data'='no', 
+                          'primary_source' = 'yes',
+                          'sox_compliance' = 'no'
+                        ); 

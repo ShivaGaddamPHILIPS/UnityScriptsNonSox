9110cb5650c8e9c1d9bd1bb4155c875bab30e763
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-27 21:33:18 +0530

    Creating PIA_Control_MM_CheckDuplInvoices/rbkp_mbp.sql

---
+++ PIA_Control_MM_CheckDuplInvoices/rbkp_mbp.sql
@@ -0,0 +1,17 @@
+
+                        -- Databricks notebook source
+                        CREATE  TABLE IF NOT EXISTS prod_l2.rbkp_mbp
+                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/RBKP_MBP'
+                        COMMENT 'rbkp_mbp';
+
+                        ALTER TABLE  prod_l2.foundation.rbkp_mbp
+                        SET TBLPROPERTIES (
+                          'delta.autoOptimize.optimizeWrite' = 'true',
+                          'delta.autoOptimize.autoCompact' = 'false',
+                          'delta.enableChangeDataFeed' = 'true',
+                          'delta.tuneFileSizesForRewrites' = 'true',
+                          'certified' = 'no',
+                          'pii_data'='no', 
+                          'primary_source' = 'yes',
+                          'sox_compliance' = 'no'
+                        ); 

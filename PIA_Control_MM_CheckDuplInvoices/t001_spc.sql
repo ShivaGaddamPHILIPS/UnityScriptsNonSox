aae4ec2880dc6ffadfb46f639ea2f5452fa234a9
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-27 21:34:20 +0530

    Creating PIA_Control_MM_CheckDuplInvoices/t001_spc.sql

---
+++ PIA_Control_MM_CheckDuplInvoices/t001_spc.sql
@@ -0,0 +1,17 @@
+
+                        -- Databricks notebook source
+                        CREATE  TABLE IF NOT EXISTS prod_l2.t001_spc
+                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T001_SPC'
+                        COMMENT 't001_spc';
+
+                        ALTER TABLE  prod_l2.foundation.t001_spc
+                        SET TBLPROPERTIES (
+                          'delta.autoOptimize.optimizeWrite' = 'true',
+                          'delta.autoOptimize.autoCompact' = 'false',
+                          'delta.enableChangeDataFeed' = 'true',
+                          'delta.tuneFileSizesForRewrites' = 'true',
+                          'certified' = 'no',
+                          'pii_data'='no', 
+                          'primary_source' = 'yes',
+                          'sox_compliance' = 'no'
+                        ); 

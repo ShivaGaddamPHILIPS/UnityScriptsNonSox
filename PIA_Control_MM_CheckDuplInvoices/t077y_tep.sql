4a604a280808d1a6352a3d83a88e772371bfb06e
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-27 21:35:24 +0530

    Creating PIA_Control_MM_CheckDuplInvoices/t077y_tep.sql

---
+++ PIA_Control_MM_CheckDuplInvoices/t077y_tep.sql
@@ -0,0 +1,17 @@
+
+                        -- Databricks notebook source
+                        CREATE  TABLE IF NOT EXISTS prod_l2.t077y_tep
+                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T077Y_TEP'
+                        COMMENT 't077y_tep';
+
+                        ALTER TABLE  prod_l2.foundation.t077y_tep
+                        SET TBLPROPERTIES (
+                          'delta.autoOptimize.optimizeWrite' = 'true',
+                          'delta.autoOptimize.autoCompact' = 'false',
+                          'delta.enableChangeDataFeed' = 'true',
+                          'delta.tuneFileSizesForRewrites' = 'true',
+                          'certified' = 'no',
+                          'pii_data'='no', 
+                          'primary_source' = 'yes',
+                          'sox_compliance' = 'no'
+                        ); 

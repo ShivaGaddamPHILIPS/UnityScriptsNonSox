6784cb24425708c521ff8925acca24da5f421f62
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-27 20:36:09 +0530

    Creating PIA_AP_Analysis/tbslt_mcp.sql

---
+++ PIA_AP_Analysis/tbslt_mcp.sql
@@ -0,0 +1,17 @@
+
+                        -- Databricks notebook source
+                        CREATE  TABLE IF NOT EXISTS prod_l2.tbslt_mcp
+                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/TBSLT_MCP'
+                        COMMENT 'tbslt_mcp';
+
+                        ALTER TABLE  prod_l2.foundation.tbslt_mcp
+                        SET TBLPROPERTIES (
+                          'delta.autoOptimize.optimizeWrite' = 'true',
+                          'delta.autoOptimize.autoCompact' = 'false',
+                          'delta.enableChangeDataFeed' = 'true',
+                          'delta.tuneFileSizesForRewrites' = 'true',
+                          'certified' = 'no',
+                          'pii_data'='no', 
+                          'primary_source' = 'yes',
+                          'sox_compliance' = 'no'
+                        ); 

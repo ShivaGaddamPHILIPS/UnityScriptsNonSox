3656a2985c70b32683fedabb10935bee20feead0
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-27 20:35:53 +0530

    Creating PIA_AP_Analysis/t074t_mcp.sql

---
+++ PIA_AP_Analysis/t074t_mcp.sql
@@ -0,0 +1,17 @@
+
+                        -- Databricks notebook source
+                        CREATE  TABLE IF NOT EXISTS prod_l2.t074t_mcp
+                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T074T_MCP'
+                        COMMENT 't074t_mcp';
+
+                        ALTER TABLE  prod_l2.foundation.t074t_mcp
+                        SET TBLPROPERTIES (
+                          'delta.autoOptimize.optimizeWrite' = 'true',
+                          'delta.autoOptimize.autoCompact' = 'false',
+                          'delta.enableChangeDataFeed' = 'true',
+                          'delta.tuneFileSizesForRewrites' = 'true',
+                          'certified' = 'no',
+                          'pii_data'='no', 
+                          'primary_source' = 'yes',
+                          'sox_compliance' = 'no'
+                        ); 

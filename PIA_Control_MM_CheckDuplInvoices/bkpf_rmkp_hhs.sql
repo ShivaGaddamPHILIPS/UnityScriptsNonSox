b0ca6793f6adfe1b02c38611e352d38fb9b3ac38
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-30 11:57:32 +0530

    Updating PIA_Control_MM_CheckDuplInvoices/bkpf_rmkp_hhs.sql

--- PIA_Control_MM_CheckDuplInvoices/bkpf_rmkp_hhs.sql
+++ PIA_Control_MM_CheckDuplInvoices/bkpf_rmkp_hhs.sql
@@ -1,17 +1,17 @@
 
-                        -- Databricks notebook source
-                        CREATE  TABLE IF NOT EXISTS prod_l2.bkpf_rmkp_hhs
-                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/BKPF_RMKP_HHS'
-                        COMMENT 'bkpf_rmkp_hhs';
+-- Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.bkpf_rmkp_hhs
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/BKPF_RMKP_HHS'
+COMMENT 'bkpf_rmkp_hhs';
 
-                        ALTER TABLE  prod_l2.foundation.bkpf_rmkp_hhs
-                        SET TBLPROPERTIES (
-                          'delta.autoOptimize.optimizeWrite' = 'true',
-                          'delta.autoOptimize.autoCompact' = 'false',
-                          'delta.enableChangeDataFeed' = 'true',
-                          'delta.tuneFileSizesForRewrites' = 'true',
-                          'certified' = 'no',
-                          'pii_data'='no', 
-                          'primary_source' = 'yes',
-                          'sox_compliance' = 'no'
+ALTER TABLE  prod_l2.foundation.bkpf_rmkp_hhs
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
                         ); 

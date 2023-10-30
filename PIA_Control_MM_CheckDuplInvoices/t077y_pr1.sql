b4981ede22405ca8e9e94e9307c516aa4fb13e53
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-30 12:09:28 +0530

    Updating PIA_Control_MM_CheckDuplInvoices/t077y_pr1.sql

--- PIA_Control_MM_CheckDuplInvoices/t077y_pr1.sql
+++ PIA_Control_MM_CheckDuplInvoices/t077y_pr1.sql
@@ -1,17 +1,17 @@
 
-                        -- Databricks notebook source
-                        CREATE  TABLE IF NOT EXISTS prod_l2.t077y_pr1
-                        LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T077Y_PR1'
-                        COMMENT 't077y_pr1';
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t077y_pr1
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T077Y_PR1'
+COMMENT 't077y_pr1';
 
-                        ALTER TABLE  prod_l2.foundation.t077y_pr1
-                        SET TBLPROPERTIES (
-                          'delta.autoOptimize.optimizeWrite' = 'true',
-                          'delta.autoOptimize.autoCompact' = 'false',
-                          'delta.enableChangeDataFeed' = 'true',
-                          'delta.tuneFileSizesForRewrites' = 'true',
-                          'certified' = 'no',
-                          'pii_data'='no', 
-                          'primary_source' = 'yes',
-                          'sox_compliance' = 'no'
-                        ); 
+ALTER TABLE  prod_l2.foundation.t077y_pr1
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

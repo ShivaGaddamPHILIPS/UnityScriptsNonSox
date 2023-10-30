11f2e991f62e4de21ff7c3d35ec3ca7f8e5af1db
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-30 12:08:02 +0530

    Updating PIA_Control_MM_CheckDuplInvoices/rbkp_hhs.sql

--- PIA_Control_MM_CheckDuplInvoices/rbkp_hhs.sql
+++ PIA_Control_MM_CheckDuplInvoices/rbkp_hhs.sql
@@ -1,6 +1,6 @@
 
 # Databricks notebook source
-CREATE  TABLE IF NOT EXISTS prod_l2.rbkp_hhs
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.rbkp_hhs
 LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/RBKP_HHS'
 COMMENT 'rbkp_hhs';
 
@@ -14,4 +14,4 @@ SET TBLPROPERTIES (
   'pii_data'='no', 
   'primary_source' = 'yes',
   'sox_compliance' = 'no'
-                        ); 
+); 

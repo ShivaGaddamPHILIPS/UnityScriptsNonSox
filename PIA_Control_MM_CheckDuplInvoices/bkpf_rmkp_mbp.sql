ebd88cd1955d33e30483d0268164e4c20868d302
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-30 12:06:41 +0530

    Updating PIA_Control_MM_CheckDuplInvoices/bkpf_rmkp_mbp.sql

--- PIA_Control_MM_CheckDuplInvoices/bkpf_rmkp_mbp.sql
+++ PIA_Control_MM_CheckDuplInvoices/bkpf_rmkp_mbp.sql
@@ -1,6 +1,6 @@
 
 # Databricks notebook source
-CREATE  TABLE IF NOT EXISTS prod_l2.bkpf_rmkp_mbp
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.bkpf_rmkp_mbp
 LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/BKPF_RMKP_MBP'
 COMMENT 'bkpf_rmkp_mbp';
 
@@ -14,4 +14,4 @@ SET TBLPROPERTIES (
   'pii_data'='no', 
   'primary_source' = 'yes',
   'sox_compliance' = 'no'
-                        ); 
+); 

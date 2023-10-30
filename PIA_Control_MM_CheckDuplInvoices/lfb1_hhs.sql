b0aaf6656154ceed802b1cf65dc0dd773e314568
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-30 12:07:34 +0530

    Updating PIA_Control_MM_CheckDuplInvoices/lfb1_hhs.sql

--- PIA_Control_MM_CheckDuplInvoices/lfb1_hhs.sql
+++ PIA_Control_MM_CheckDuplInvoices/lfb1_hhs.sql
@@ -1,6 +1,6 @@
 
 # Databricks notebook source
-CREATE  TABLE IF NOT EXISTS prod_l2.lfb1_hhs
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.lfb1_hhs
 LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/LFB1_HHS'
 COMMENT 'lfb1_hhs';
 
@@ -14,4 +14,4 @@ SET TBLPROPERTIES (
   'pii_data'='no', 
   'primary_source' = 'yes',
   'sox_compliance' = 'no'
-                        ); 
+); 

6810e482e38f6325fa23cdf9f46692edc51ac2dd
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-30 12:08:31 +0530

    Updating PIA_Control_MM_CheckDuplInvoices/rbkp_tep.sql

--- PIA_Control_MM_CheckDuplInvoices/rbkp_tep.sql
+++ PIA_Control_MM_CheckDuplInvoices/rbkp_tep.sql
@@ -1,6 +1,6 @@
 
 # Databricks notebook source
-CREATE  TABLE IF NOT EXISTS prod_l2.rbkp_tep
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.rbkp_tep
 LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/RBKP_TEP'
 COMMENT 'rbkp_tep';
 
@@ -14,4 +14,4 @@ SET TBLPROPERTIES (
   'pii_data'='no', 
   'primary_source' = 'yes',
   'sox_compliance' = 'no'
-                        ); 
+); 

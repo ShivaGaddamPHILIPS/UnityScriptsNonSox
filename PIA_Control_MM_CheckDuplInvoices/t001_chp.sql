1c98ec7f359d5e5485723b6d8d41b5d734a4cb4d
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-10-30 12:08:35 +0530

    Updating PIA_Control_MM_CheckDuplInvoices/t001_chp.sql

--- PIA_Control_MM_CheckDuplInvoices/t001_chp.sql
+++ PIA_Control_MM_CheckDuplInvoices/t001_chp.sql
@@ -1,6 +1,6 @@
 
 # Databricks notebook source
-CREATE  TABLE IF NOT EXISTS prod_l2.t001_chp
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t001_chp
 LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T001_CHP'
 COMMENT 't001_chp';
 
@@ -14,4 +14,4 @@ SET TBLPROPERTIES (
   'pii_data'='no', 
   'primary_source' = 'yes',
   'sox_compliance' = 'no'
-                        ); 
+); 

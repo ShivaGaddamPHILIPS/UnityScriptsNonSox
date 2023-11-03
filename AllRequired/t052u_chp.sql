857948be8baf382d08122bcf87dafd926ead9cd7
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:57:23 +0530

    Updating AllRequired/t052u_chp.sql

--- AllRequired/t052u_chp.sql
+++ AllRequired/t052u_chp.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t052u_chp
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T052U_CHP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T052U_CHP'
 COMMENT 't052u_chp';
 
 ALTER TABLE  prod_l2.foundation.t052u_chp

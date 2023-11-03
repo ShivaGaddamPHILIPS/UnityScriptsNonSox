125a938b2deed416302eb0227c2248f910809d12
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:57:42 +0530

    Updating AllRequired/t074t_chp.sql

--- AllRequired/t074t_chp.sql
+++ AllRequired/t074t_chp.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t074t_chp
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T074T_CHP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T074T_CHP'
 COMMENT 't074t_chp';
 
 ALTER TABLE  prod_l2.foundation.t074t_chp

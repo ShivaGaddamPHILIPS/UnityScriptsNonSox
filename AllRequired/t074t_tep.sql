2f33a865f4fcf5a066b198086955b07a70f39a44
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:12:09 +0530

    Updating AllRequired/t074t_tep.sql

--- AllRequired/t074t_tep.sql
+++ AllRequired/t074t_tep.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t074t_tep
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T074T_TEP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T074T_TEP'
 COMMENT 't074t_tep';
 
 ALTER TABLE  prod_l2.foundation.t074t_tep

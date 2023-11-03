9db8e832b9c093ce763466a433c1de03878a9496
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 16:56:00 +0530

    Creating AllRequired/skat_mbp.sql

---
+++ AllRequired/skat_mbp.sql
@@ -0,0 +1,17 @@
+
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.skat_mbp
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//POCA/QVDs/SKAT_MBP'
+COMMENT 'skat_mbp';
+
+ALTER TABLE  prod_l2.foundation.skat_mbp
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

ab5824b71d41ae676297dfe2faceda9ac44ea14a
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:02:20 +0530

    Creating AllRequired/cdhdr_mdchglog_mcp_20220101_20220131.sql

---
+++ AllRequired/cdhdr_mdchglog_mcp_20220101_20220131.sql
@@ -0,0 +1,17 @@
+
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.cdhdr_mdchglog_mcp_20220101_20220131
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//ICS_Ctrls/QVDs/CDHDR_MDCHGLOG_MCP_20220101_20220131'
+COMMENT 'cdhdr_mdchglog_mcp_20220101_20220131';
+
+ALTER TABLE  prod_l2.foundation.cdhdr_mdchglog_mcp_20220101_20220131
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

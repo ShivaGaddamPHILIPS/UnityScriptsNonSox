c8cba042340064260529cd3e25360b2437444692
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:20:39 +0530

    Updating AllRequired/tcurx_chp.sql

--- AllRequired/tcurx_chp.sql
+++ AllRequired/tcurx_chp.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.tcurx_chp
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/TCURX_CHP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/TCURX_CHP'
 COMMENT 'tcurx_chp';
 
 ALTER TABLE  prod_l2.foundation.tcurx_chp

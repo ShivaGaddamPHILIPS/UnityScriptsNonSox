fac6802323a20a1a338173dbdd8df0619c913264
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:34:15 +0530

    Updating AllRequired/t074t_hhs.sql

--- AllRequired/t074t_hhs.sql
+++ AllRequired/t074t_hhs.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t074t_hhs
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T074T_HHS'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T074T_HHS'
 COMMENT 't074t_hhs';
 
 ALTER TABLE  prod_l2.foundation.t074t_hhs

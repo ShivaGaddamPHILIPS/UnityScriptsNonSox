baf22d0006c6fc315e5845e75acbf757ce415854
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:57:40 +0530

    Updating AllRequired/t074t_cep.sql

--- AllRequired/t074t_cep.sql
+++ AllRequired/t074t_cep.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t074t_cep
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T074T_CEP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T074T_CEP'
 COMMENT 't074t_cep';
 
 ALTER TABLE  prod_l2.foundation.t074t_cep

00bab640133e8c6b6d2011a9bb3dfe51350e8ceb
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:18:03 +0530

    Updating AllRequired/t001_spc.sql

--- AllRequired/t001_spc.sql
+++ AllRequired/t001_spc.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t001_spc
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T001_SPC'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T001_SPC'
 COMMENT 't001_spc';
 
 ALTER TABLE  prod_l2.foundation.t001_spc

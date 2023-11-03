4dca131808cd829122760b58e0e068a98243d6ed
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:04:09 +0530

    Creating AllRequired/cdpos_mdchglog_pr1_20220301_20220331.sql

---
+++ AllRequired/cdpos_mdchglog_pr1_20220301_20220331.sql
@@ -0,0 +1,17 @@
+
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.cdpos_mdchglog_pr1_20220301_20220331
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//ICS_Ctrls/QVDs/CDPOS_MDCHGLOG_PR1_20220301_20220331'
+COMMENT 'cdpos_mdchglog_pr1_20220301_20220331';
+
+ALTER TABLE  prod_l2.foundation.cdpos_mdchglog_pr1_20220301_20220331
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

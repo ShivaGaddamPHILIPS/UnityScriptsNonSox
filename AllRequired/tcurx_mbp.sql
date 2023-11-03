adbc83e3ccaa3f14e0578b1f55ee9ebdc261e71b
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:29:57 +0530

    Updating AllRequired/tcurx_mbp.sql

--- AllRequired/tcurx_mbp.sql
+++ AllRequired/tcurx_mbp.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.tcurx_mbp
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/TCURX_MBP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/TCURX_MBP'
 COMMENT 'tcurx_mbp';
 
 ALTER TABLE  prod_l2.foundation.tcurx_mbp

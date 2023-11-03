a7f1550d13dc82fd987be11d0d8734e1ad73ff40
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:07:25 +0530

    Creating AllRequired/pil_knkk_fscm_previous.sql

---
+++ AllRequired/pil_knkk_fscm_previous.sql
@@ -0,0 +1,17 @@
+
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.pil_knkk_fscm_previous
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//CMAR/QVDs/PIL_KNKK_FSCM_Previous'
+COMMENT 'pil_knkk_fscm_previous';
+
+ALTER TABLE  prod_l2.foundation.pil_knkk_fscm_previous
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

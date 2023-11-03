59e119d8e761ae0d3e9673c643e86ff582603e8d
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 16:57:23 +0530

    Creating AllRequired/t001_mp1.sql

---
+++ AllRequired/t001_mp1.sql
@@ -0,0 +1,17 @@
+
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t001_mp1
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//POCA/QVDs/T001_MP1'
+COMMENT 't001_mp1';
+
+ALTER TABLE  prod_l2.foundation.t001_mp1
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

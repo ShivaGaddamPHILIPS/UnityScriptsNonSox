7cf9a9db1bf1ce36763b41e2020687746007e9ad
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:11:39 +0530

    Updating AllRequired/tbslt_hhs.sql

--- AllRequired/tbslt_hhs.sql
+++ AllRequired/tbslt_hhs.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.tbslt_hhs
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/TBSLT_HHS'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/TBSLT_HHS'
 COMMENT 'tbslt_hhs';
 
 ALTER TABLE  prod_l2.foundation.tbslt_hhs

7cb154fa77edddeb09dde22aaa2f73c2346beaa1
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 16:55:53 +0530

    Creating AllRequired/skat_chp.sql

---
+++ AllRequired/skat_chp.sql
@@ -0,0 +1,17 @@
+
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.skat_chp
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//POCA/QVDs/SKAT_CHP'
+COMMENT 'skat_chp';
+
+ALTER TABLE  prod_l2.foundation.skat_chp
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

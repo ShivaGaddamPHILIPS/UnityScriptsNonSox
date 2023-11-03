243a09a67257e6d043217e8b87fdf14c4759f92f
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:12:26 +0530

    Updating AllRequired/tcurx_spc.sql

--- AllRequired/tcurx_spc.sql
+++ AllRequired/tcurx_spc.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.tcurx_spc
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/TCURX_SPC'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/TCURX_SPC'
 COMMENT 'tcurx_spc';
 
 ALTER TABLE  prod_l2.foundation.tcurx_spc

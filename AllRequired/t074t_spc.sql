febbc2fed3a6efc858bb28b5e7d98b2dec3c75cc
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:12:06 +0530

    Updating AllRequired/t074t_spc.sql

--- AllRequired/t074t_spc.sql
+++ AllRequired/t074t_spc.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t074t_spc
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T074T_SPC'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T074T_SPC'
 COMMENT 't074t_spc';
 
 ALTER TABLE  prod_l2.foundation.t074t_spc

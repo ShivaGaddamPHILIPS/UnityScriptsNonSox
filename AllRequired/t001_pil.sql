1fb55c24cf86427073b690a537ebc5eb6d2c77dd
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:19:56 +0530

    Updating AllRequired/t001_pil.sql

--- AllRequired/t001_pil.sql
+++ AllRequired/t001_pil.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t001_pil
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T001_PIL'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T001_PIL'
 COMMENT 't001_pil';
 
 ALTER TABLE  prod_l2.foundation.t001_pil

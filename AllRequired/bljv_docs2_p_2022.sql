2c51e26c2f125e552bef49a7e4fa3186a31915a1
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:26:02 +0530

    Creating AllRequired/bljv_docs2_p_2022.sql

---
+++ AllRequired/bljv_docs2_p_2022.sql
@@ -0,0 +1,17 @@
+
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.bljv_docs2_p_2022
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//ODW/QVDs/BLJV_Docs2_P_2022'
+COMMENT 'bljv_docs2_p_2022';
+
+ALTER TABLE  prod_l2.foundation.bljv_docs2_p_2022
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

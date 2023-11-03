a1f846312aea1576603f91de2ce962513fb731d9
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:58:16 +0530

    Updating AllRequired/tcurx_cep.sql

--- AllRequired/tcurx_cep.sql
+++ AllRequired/tcurx_cep.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.tcurx_cep
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/TCURX_CEP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/TCURX_CEP'
 COMMENT 'tcurx_cep';
 
 ALTER TABLE  prod_l2.foundation.tcurx_cep

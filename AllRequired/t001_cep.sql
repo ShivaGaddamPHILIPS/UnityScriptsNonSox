8a9e25e11fc9f6ea7c40239582a973a72c6b173a
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:06:57 +0530

    Updating AllRequired/t001_cep.sql

--- AllRequired/t001_cep.sql
+++ AllRequired/t001_cep.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t001_cep
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//POCA/QVDs/T001_CEP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T001_CEP'
 COMMENT 't001_cep';
 
 ALTER TABLE  prod_l2.foundation.t001_cep

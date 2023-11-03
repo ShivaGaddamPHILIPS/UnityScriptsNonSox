f3a57f948ff719669e3dd9a9d3ce52cad9e2ef77
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:30:47 +0530

    Creating AllRequired/usr02_tep.sql

---
+++ AllRequired/usr02_tep.sql
@@ -0,0 +1,17 @@
+
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.usr02_tep
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//MJV/QVDs/TEP/USR02_TEP'
+COMMENT 'usr02_tep';
+
+ALTER TABLE  prod_l2.foundation.usr02_tep
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

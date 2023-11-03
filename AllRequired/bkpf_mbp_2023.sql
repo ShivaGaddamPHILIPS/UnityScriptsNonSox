0ab9e302858eafd2b2c3748bac2553aa5fdd61fd
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:15:09 +0530

    Creating AllRequired/bkpf_mbp_2023.sql

---
+++ AllRequired/bkpf_mbp_2023.sql
@@ -0,0 +1,17 @@
+
+# Databricks notebook source
+CREATE  TABLE IF NOT EXISTS prod_l2.foundation.bkpf_mbp_2023
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//MJV/QVDs/MBP/BKPF_MBP_2023'
+COMMENT 'bkpf_mbp_2023';
+
+ALTER TABLE  prod_l2.foundation.bkpf_mbp_2023
+SET TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'false',
+  'delta.enableChangeDataFeed' = 'true',
+  'delta.tuneFileSizesForRewrites' = 'true',
+  'certified' = 'no',
+  'pii_data'='no', 
+  'primary_source' = 'yes',
+  'sox_compliance' = 'no'
+); 

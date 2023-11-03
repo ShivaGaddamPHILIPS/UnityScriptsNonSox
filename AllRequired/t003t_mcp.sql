60ea9010d32864b1aad16c31e0c09b56c3308948
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 17:09:49 +0530

    Updating AllRequired/t003t_mcp.sql

--- AllRequired/t003t_mcp.sql
+++ AllRequired/t003t_mcp.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t003t_mcp
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T003T_MCP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T003T_MCP'
 COMMENT 't003t_mcp';
 
 ALTER TABLE  prod_l2.foundation.t003t_mcp

6bc245ce9d98f0b9927f9c63981ed212c257c200
ShivaGaddamPHILIPS <146947540+ShivaGaddamPHILIPS@users.noreply.github.com>
2023-11-03 18:28:58 +0530

    Updating AllRequired/t003t_cep.sql

--- AllRequired/t003t_cep.sql
+++ AllRequired/t003t_cep.sql
@@ -1,7 +1,7 @@
 
 # Databricks notebook source
 CREATE  TABLE IF NOT EXISTS prod_l2.foundation.t003t_cep
-LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSID/QVDs/T003T_CEP'
+LOCATION 'abfss://silver@az21p1datalakewe.dfs.core.windows.net/im/L2/Jupiter//BSIK/QVDs/T003T_CEP'
 COMMENT 't003t_cep';
 
 ALTER TABLE  prod_l2.foundation.t003t_cep
